62ae40521932b27b6c3016bb32b351269700e702
Patrick Kox <patrick.kox@linux.com>
2021-09-12 20:58:27 +0200

    Replaced GB transation with US (my system locale is set to GB, so this makes it easier to test the Dutch language version. Also switched to QMake from CMake since CMake gave errors when running QT Linguist to create/update the .ts files

--- HoofdpijnAgenda/HoofdpijnAgenda_en_US.ts
+++ HoofdpijnAgenda/HoofdpijnAgenda_en_US.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl_BE">
+<TS version="2.1" language="en_US">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <source>MainWindow</source>
-        <translation>Hoofdpijn Agenda</translation>
+        <translation>Headache Agenda</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="28"/>
         <source>Bestand</source>
-        <translation>&amp;Bestand</translation>
+        <translation>&amp;File</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="33"/>
         <source>Hulpmiddelen</source>
-        <translation>&amp;Hulpmiddellen</translation>
+        <translation>&amp;Tools</translation>
     </message>
 </context>
 </TS>
---
+++ build-HoofdpijnAgenda-Desktop_Qt_6_0_4_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,75 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.0.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QMenu *menuBestand;
+    QMenu *menuHulpmiddelen;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 27));
+        menuBestand = new QMenu(menubar);
+        menuBestand->setObjectName(QString::fromUtf8("menuBestand"));
+        menuHulpmiddelen = new QMenu(menubar);
+        menuHulpmiddelen->setObjectName(QString::fromUtf8("menuHulpmiddelen"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuBestand->menuAction());
+        menubar->addAction(menuHulpmiddelen->menuAction());
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        menuBestand->setTitle(QCoreApplication::translate("MainWindow", "&Bestand", nullptr));
+        menuHulpmiddelen->setTitle(QCoreApplication::translate("MainWindow", "&Hulpmiddellen", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
